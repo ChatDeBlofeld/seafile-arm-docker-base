93b070470b6b2f37f7ca6426e3d648672535a78e
Olof <olof.nord@tutanota.com>
2022-01-24 22:05:03 +0100

    Make wait_for_db script more verbose (#24)
    * Set wait_for_db script to print error message, if not expected error code is found
    
    * Additional logging when waiting for db
    
    Co-authored-by: Thibaud Franchetti <noreply@franchetti.ch>

--- scripts/wait_for_db.sh
+++ scripts/wait_for_db.sh
@@ -11,11 +11,15 @@ python3 - <<PYTHON_SCRIPT
 import MySQLdb
 from time import sleep
 
+c = 1
 while True:
     try:
         db=MySQLdb.connect(host="${MYSQL_HOST}", port=${MYSQL_PORT})
     except MySQLdb.OperationalError as err:
         if err.args[0] == ${EXPECTED_ERROR_CODE}:
             break
+        elif not c % 10:
+            print("Still waiting... Is [${MYSQL_HOST}:${MYSQL_PORT}] reachable from within the container?")
+    c+= 1
     sleep(1)
 PYTHON_SCRIPT
